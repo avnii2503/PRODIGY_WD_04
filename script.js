b6349fa3ee17d7fc50b340a4b64230578b825253
avnii2503 <113117646+avnii2503@users.noreply.github.com>
2023-11-30 01:01:06 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', smoothScroll);
+    });
+
+    function smoothScroll(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scroll({
+            top: targetElement.offsetTop - 20,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+});
